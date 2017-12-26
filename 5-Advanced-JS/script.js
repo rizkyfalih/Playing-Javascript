23c4ec237b838d93b28c2219854875f79d8511d3
Rizky <rizkyfalihpridyandhika@ymail.com>
2017-12-26 07:02:17 +0700

    Funtions and object

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -29,3 +29,143 @@ mark.calculateAge();
 console.log(jane.lastName);
 console.log(john.lastName);
 console.log(mark.lastName);
+*/
+
+
+
+
+// Object.create
+/*
+var personProto = {
+    calculateAge: function(){
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'Teacher';
+
+var jane =  Object.create(personProto, {
+    name: { value: 'Jane'},
+    yearOfBirth: { value: 1969},
+    job: { value: 'Designer'}
+});
+*/
+
+
+
+// Primitives vs Objects
+/*
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+//Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a,b){
+    a = 30;
+    b.city = 'San Fransisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+
+//////////////////////////////////////////////
+// Lecture: Passing functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn){
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function isFullAge(el){
+    return el >=18;
+}
+
+function calculateAge(el){
+    return 2016 - el;
+}
+
+function maxHeartRate(el){
+    if (el >= 18 && el <= 81){
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+    
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var maxHeartRate = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(maxHeartRate);
+*/
+
+
+
+//////////////////////////////////////////////
+// Lecture: Functions returning functions
+/*
+function interviewQuestion(job){
+    if ( job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if ( job === 'teacher'){
+        return function(name){
+            console.log(name + ', what subject do you teach?');
+        }
+    } else {
+        return function(name){
+            console.log('Hello ' + name + ", what do you do?");
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+
+interviewQuestion('teacher')('Mark');
+*/
+
+
+
+
+//////////////////////////////////////////////
+// Lecture: IIFE
+
