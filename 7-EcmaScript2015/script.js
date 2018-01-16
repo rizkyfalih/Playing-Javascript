632b9ecfd198b0ea6a4135b7642d7bfad029eb7b
Rizky <rizkyfalihpridyandhika@ymail.com>
2018-01-16 15:43:43 +0700

    added spread operator, rest parameters, and default parameter

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -269,7 +269,7 @@ console.log(retirement);
 
 
 //////////////////////////////////////////////////
-// Lecture: Destructuring
+// Lecture: Array
 
 /*
 const boxes = document.querySelectorAll('.box');
@@ -329,3 +329,133 @@ console.log(ages[full.indexOf(true)]);
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
 */
+
+
+
+
+//////////////////////////////////////////////////
+// Lecture: Spread Operator
+
+/*
+function addFourAges (a, b, c, d){
+    return a+b+c+d;
+}
+
+var sum1 = addFourAges(18, 20, 12, 21);
+console.log(sum1);
+
+// ES5
+var ages = [18, 20, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Marry', 'Bob','Ann'];
+const bigFamily = [...familySmith, ...familyMiller];
+console.log(bigFamily);
+
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+*/
+
+
+
+
+//////////////////////////////////////////////////
+// Lecture: Rest parameters
+
+/*
+//ES5
+function isFullAge5(){
+    //console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+
+    argsArr.forEach(function(cur){
+        console.log((2016-cur) >= 18);
+    })
+}
+
+//isFullAge5(1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1897);
+
+
+//ES6
+function isFullAge6(...years) {
+    years.forEach(cur => console.log((2016 - cur) >= 18));
+}
+
+isFullAge6(1990, 1999, 1965);
+*/
+
+/*
+//ES5
+function isFullAge5(limit){
+    //console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+
+    argsArr.forEach(function(cur){
+        console.log((2016-cur) >= limit);
+    })
+}
+
+isFullAge5(16, 1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1897);
+
+
+//ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2016 - cur) >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965);
+*/
+
+
+
+
+//////////////////////////////////////////////////
+// Lecture: Default parameters
+
+/*
+//ES5
+function SmithPerson(firstName, yearOfBirth, lastName, nationality){
+
+    lastName === undefined ? lastName = 'Smith' : lastName;
+    nationality === undefined ? nationality = 'american' : nationality;
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+*/
+
+/*
+//ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american'){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1993, 'Diaz', 'Spanish')
+*/
+
+
+
+
+
+//////////////////////////////////////////////////
+// Lecture: Default parameters
